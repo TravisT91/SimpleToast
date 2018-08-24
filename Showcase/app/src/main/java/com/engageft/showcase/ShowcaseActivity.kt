075352da146c0903a940fdbafb6234736babbbae
Joseph Hutchins <joseph.hutchins@engageft.com>
2018-08-24 11:42:49 -0700

    Update and finish documenting.

--- Showcase/app/src/main/java/com/engageft/showcase/ShowcaseActivity.kt
+++ Showcase/app/src/main/java/com/engageft/showcase/ShowcaseActivity.kt
@@ -1,15 +1,12 @@
 package com.engageft.showcase
 
 import android.os.Bundle
-import android.util.Log
 import androidx.navigation.fragment.NavHostFragment
 import com.engageft.apptoolbox.LotusActivity
 
 class ShowcaseActivity : LotusActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        Log.e("Joey", "Hello world!")
     }
 
     override fun instantiateNavHostFragment(): NavHostFragment {
