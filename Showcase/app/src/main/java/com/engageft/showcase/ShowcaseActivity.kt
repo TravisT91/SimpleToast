88c2d12a405482c06cdfb23dbd873bb3d8629654
Joseph Hutchins <joseph.hutchins@engageft.com>
2018-09-04 10:21:30 -0700

    Migrate EK to androidX.

--- Showcase/app/src/main/java/com/engageft/showcase/ShowcaseActivity.kt
+++ Showcase/app/src/main/java/com/engageft/showcase/ShowcaseActivity.kt
@@ -1,15 +1,15 @@
 package com.engageft.showcase
 
 import android.os.Bundle
-import androidx.navigation.fragment.NavHostFragment
 import com.engageft.apptoolbox.LotusActivity
+import com.engageft.apptoolbox.LotusActivityConfig
 
 class ShowcaseActivity : LotusActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
 
-    override fun instantiateNavHostFragment(): NavHostFragment {
-        return NavHostFragment.create(R.navigation.nav_graph)
+    override fun getLotusActivityConfig(): LotusActivityConfig {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 }
