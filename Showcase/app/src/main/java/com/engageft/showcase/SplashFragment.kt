3db459e9b498a48b8e7a179431dfad6cf70e2963
Joseph Hutchins <joseph.hutchins@engageft.com>
2018-09-06 15:11:39 -0700

    SHOW-135: Finish up example usage of InputWithLabel.

--- Showcase/app/src/main/java/com/engageft/showcase/SplashFragment.kt
+++ Showcase/app/src/main/java/com/engageft/showcase/SplashFragment.kt
@@ -10,7 +10,9 @@ import androidx.navigation.findNavController
 import com.engageft.apptoolbox.BaseToolbarConfig
 import com.engageft.apptoolbox.LotusFullScreenFragment
 import com.engageft.apptoolbox.LotusFullScreenFragmentConfig
+import com.engageft.apptoolbox.view.EmailInputWithLabel
 import com.engageft.apptoolbox.view.PasswordInputWithLabel
+import com.engageft.apptoolbox.view.PhoneInputWithLabel
 import com.engageft.apptoolbox.view.TextInputWithLabel
 
 /**
@@ -35,20 +37,24 @@ class SplashFragment : LotusFullScreenFragment() {
         button.setOnClickListener{ view.findNavController().navigate(R.id.action_splash_fragment_to_get_started_fragment) }
 
         val input1 = view.findViewById<TextInputWithLabel>(R.id.input1)
-        //input1.setHelperText("This is helper text.")
-        //input1.setError("This is an error.\nThis is a second error.\nThis is a third error.\nThis is a fourth error.")
-        //input1.setClearIcon(R.drawable.ic_clear)
         input1.setClearIcon(ContextCompat.getDrawable(activity!!, R.drawable.ic_clear))
+        input1.setClearIconTint(R.color.warning)
 
 
         val input2 = view.findViewById<PasswordInputWithLabel>(R.id.input2)
-        input2.setInputText("This is helper text.")
+        input2.setHelperText("put a password!")
 
-        val input3 = view.findViewById<TextInputWithLabel>(R.id.input3)
+        val input3 = view.findViewById<PhoneInputWithLabel>(R.id.input3)
         input3.setClearIcon(ContextCompat.getDrawable(activity!!, R.drawable.ic_clear))
+        input3.setClearIconTint(R.color.black)
+        input3.setInputTextAppearance(R.style.HelperText)
+        input3.setLabelText("I am a phone input!")
 
-        val input4 = view.findViewById<TextInputWithLabel>(R.id.input4)
-        input4.setClearIcon(ContextCompat.getDrawable(activity!!, R.drawable.ic_clear))
+        val input4 = view.findViewById<EmailInputWithLabel>(R.id.input4)
+        input4.setErrorTextAppearance(R.style.ErrorText)
+        input4.setHelperTextAppearance(R.style.HelperText)
+        input4.setHelperText("You won't see me!")
+        input4.setError("I am an error!")
         return view
     }
 }
