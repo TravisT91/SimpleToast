d11d98a2915fb8aa6c93f361cce3887eb375a45b
Joseph Hutchins <joseph.hutchins@engageft.com>
2018-09-13 15:14:37 -0700

    Undo unnecessary changes.

--- Showcase/app/src/main/java/com/engageft/showcase/ActivityWithNavigation.kt
+++ Showcase/app/src/main/java/com/engageft/showcase/ActivityWithNavigation.kt
@@ -7,7 +7,7 @@ import com.engageft.apptoolbox.LotusActivityConfig
 
 class ActivityWithNavigation : LotusActivity() {
     private val lotusActivityConfig = object : LotusActivityConfig() {
-        override val navigationMenuResourceId = R.menu.test_navigation
+        override val navigationMenuResourceId = R.menu.menu_navigation
         override val navigationGraphResourceId = R.navigation.activity_with_navigation_graph
     }
 
