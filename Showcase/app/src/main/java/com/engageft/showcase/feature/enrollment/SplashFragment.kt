93628d5dbeb4a683ea4122c3ebdc89fa1973ee92
Joseph Hutchins <joseph.hutchins@engageft.com>
2018-10-16 11:38:20 -0700

    Fix build.

--- Showcase/app/src/main/java/com/engageft/showcase/feature/enrollment/SplashFragment.kt
+++ Showcase/app/src/main/java/com/engageft/showcase/feature/enrollment/SplashFragment.kt
@@ -33,8 +33,8 @@ class SplashFragment : LotusFullScreenFragment() {
         // It is reset to normal when navigation occurs.
         view.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN or View.SYSTEM_UI_FLAG_IMMERSIVE or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
 
-        val model = ViewModelProviders.of(this).get(SplashScreenViewModel::class.java)
-        model.navigationObservable.observe(this, Observer { splashNavigationEvent : SplashScreenViewModel.SplashNavigationEvent ->
+
+        (viewModel as SplashScreenViewModel).navigationObservable.observe(this, Observer { splashNavigationEvent : SplashScreenViewModel.SplashNavigationEvent ->
             val navDestinationId = when (splashNavigationEvent) {
                 SplashScreenViewModel.SplashNavigationEvent.NOT_LOGGED_IN -> {
                     R.id.action_splash_fragment_to_login_fragment
