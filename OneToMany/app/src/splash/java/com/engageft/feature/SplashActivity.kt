3d3102c19dc8d722a95dbfca83728de740958e2e
Atia <atia.hashimi@engageft.com>
2018-10-25 17:34:57 -0700

    show-347, added class header

--- OneToMany/app/src/splash/java/com/engageft/feature/SplashActivity.kt
+++ OneToMany/app/src/splash/java/com/engageft/feature/SplashActivity.kt
@@ -3,7 +3,14 @@ package com.engageft.feature
 import com.engageft.apptoolbox.LotusActivity
 import com.engageft.apptoolbox.LotusActivityConfig
 import com.engageft.onetomany.R
-
+/**
+ * SplashActivity
+ * <p>
+ *    Handles showing of splash screen
+ * </p>
+ * Created by Atia Hashimi on 10/25/18.
+ * Copyright (c) 2018 Engage FT. All rights reserved.
+ */
 class SplashActivity: LotusActivity() {
     private val lotusActivityConfig = object : LotusActivityConfig() {
         override val navigationMenuResourceId = 0
