4d81a90d3f640fb8f95cea91b89601b0af3bd1c1
Atia <atia.hashimi@engageft.com>
2018-11-08 11:34:40 -0800

    SHOW-381, changed icon padding, and setting of imageView src.

--- OneToMany/app/src/welcome/java/com/engageft/feature/Welcome2Fragment.kt
+++ OneToMany/app/src/welcome/java/com/engageft/feature/Welcome2Fragment.kt
@@ -4,12 +4,12 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
 import com.engageft.apptoolbox.BaseViewModel
 import com.engageft.apptoolbox.LotusFullScreenFragment
 import com.engageft.onetomany.R
 import com.engageft.onetomany.databinding.Welcome2FragmentBinding
+
 /**
  * Welcome2Fragment
  *
@@ -29,7 +29,7 @@ class Welcome2Fragment: LotusFullScreenFragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         binding = DataBindingUtil.inflate(inflater, R.layout.welcome2_fragment, container, false)
 
-        binding.imageViewIcon.background = ContextCompat.getDrawable(context!!, R.drawable.welcome2_icon_background)
+        binding.imageViewIcon.setImageResource(R.drawable.welcome2_icon_background)
         binding.titleTextView.text = getString(R.string.welcome_title2)
         binding.messageTextView.text = getString(R.string.welcome_message2)
         return binding.root
--- OneToMany/app/src/welcome/java/com/engageft/feature/Welcome5Fragment.kt
+++ OneToMany/app/src/welcome/java/com/engageft/feature/Welcome5Fragment.kt
@@ -4,12 +4,12 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
 import com.engageft.apptoolbox.BaseViewModel
 import com.engageft.apptoolbox.LotusFullScreenFragment
 import com.engageft.onetomany.R
 import com.engageft.onetomany.databinding.Welcome2FragmentBinding
+
 /**
  * Welcome5Fragment
  *
@@ -29,7 +29,7 @@ class Welcome5Fragment: LotusFullScreenFragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         binding = DataBindingUtil.inflate(inflater, R.layout.welcome2_fragment, container, false)
 
-        binding.imageViewIcon.background = ContextCompat.getDrawable(context!!, R.drawable.welcome5_icon_background)
+        binding.imageViewIcon.setImageResource(R.drawable.welcome5_icon_background)
         binding.titleTextView.text = getString(R.string.welcome_title5)
         binding.messageTextView.text = getString(R.string.welcome_message5)
         return binding.root
