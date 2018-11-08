9830c19bb903cb340485074c61887313eb4ff8a8
Atia <atia.hashimi@engageft.com>
2018-11-08 08:27:04 -0800

    SHOW-364, added doc.

--- OneToMany/app/src/common/java/com/engageft/feature/BaseEngageViewModel.kt
+++ OneToMany/app/src/common/java/com/engageft/feature/BaseEngageViewModel.kt
@@ -6,7 +6,14 @@ import java.net.ConnectException
 import java.net.SocketTimeoutException
 import java.net.UnknownHostException
 
-
+/**
+ * BaseEngageViewModel
+ * <p>
+ * BaseEngageViewModel for all other viewModel to inherit from
+ * </p>
+ * Created by Atia Hashimi on 11/7/18.
+ * Copyright (c) 2018 Engage FT. All rights reserved.
+ */
 open class BaseEngageViewModel: BaseViewModel() {
 
     val dialogInfoObservable: MutableLiveData<DialogInfo> = MutableLiveData()
