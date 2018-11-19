0434c266904c59619470902447d3a07fa339649c
Atia <atia.hashimi@engageft.com>
2018-11-19 08:50:33 -0800

    SHOW-259, added app-version in acct settings

--- OneToMany/app/src/account/java/com/engageft/fis/pscu/feature/AccountSettingsFragment.kt
+++ OneToMany/app/src/account/java/com/engageft/fis/pscu/feature/AccountSettingsFragment.kt
@@ -12,9 +12,11 @@ import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.engageft.apptoolbox.BaseViewModel
 import com.engageft.apptoolbox.LotusFullScreenFragment
+import com.engageft.fis.pscu.BuildConfig
 import com.engageft.fis.pscu.NotAuthenticatedActivity
 import com.engageft.fis.pscu.R
 import com.engageft.fis.pscu.databinding.FragmentAccountBinding
+import kotlinx.android.synthetic.main.fragment_account.*
 
 /**
  * AccountSettingsFragment
@@ -64,6 +66,8 @@ class AccountSettingsFragment : LotusFullScreenFragment() {
             disclosures.setOnClickListener {
                 //TODO(ttkachuk) implement onCLick
             }
+
+            softwareVersionTextView.text = String.format(getString(R.string.software_version_format), BuildConfig.VERSION_NAME)
         }
         accountSettingsViewModel.navigationObservable.observe(this, Observer { navigationEvent ->
             when (navigationEvent) {
