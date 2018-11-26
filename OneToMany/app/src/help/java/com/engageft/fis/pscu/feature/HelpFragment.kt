7f2d3f0a8896f136274a19c7044f17adf302c917
Atia <atia.hashimi@engageft.com>
2018-11-26 13:37:34 -0800

    SHOW-362, launch cardPinFragment

--- OneToMany/app/src/help/java/com/engageft/fis/pscu/feature/HelpFragment.kt
+++ OneToMany/app/src/help/java/com/engageft/fis/pscu/feature/HelpFragment.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.fragment.findNavController
 import com.engageft.apptoolbox.BaseViewModel
 import com.engageft.apptoolbox.LotusFullScreenFragment
 import com.engageft.fis.pscu.R
@@ -23,6 +25,9 @@ class HelpFragment : LotusFullScreenFragment() {
     }
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.fragment_help, container, false)
+        view.findViewById<Button>(R.id.button).setOnClickListener {
+            findNavController().navigate(R.id.action_helpFragment_to_cardPinFragment)
+        }
         return view
     }
 }
