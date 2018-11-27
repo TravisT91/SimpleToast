26f23299d7409a89b0ad5c01929f5c0e824cad7a
Atia <atia.hashimi@engageft.com>
2018-11-27 15:31:51 -0800

    SHOW-362, completed pin card flow.

---
+++ OneToMany/app/src/onboarding/java/com/engageft/fis/pscu/feature/CardPinFragment.kt
@@ -0,0 +1,155 @@
+package com.engageft.fis.pscu.feature
+
+import android.graphics.drawable.Drawable
+import android.os.Bundle
+import android.os.Handler
+import android.view.*
+import android.widget.ImageView
+import androidx.annotation.ColorInt
+import androidx.core.content.ContextCompat
+import androidx.core.graphics.drawable.DrawableCompat
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import androidx.navigation.findNavController
+import com.engageft.apptoolbox.BaseViewModel
+import com.engageft.apptoolbox.LotusFullScreenFragment
+import com.engageft.fis.pscu.R
+import com.engageft.fis.pscu.config.EngageAppConfig
+import com.engageft.fis.pscu.databinding.FragmentCardPinBinding
+import com.redmadrobot.inputmask.MaskedTextChangedListener
+
+class CardPinFragment : LotusFullScreenFragment() {
+
+    private lateinit var binding: FragmentCardPinBinding
+    private lateinit var cardPinViewModel: CardPinViewModel
+    private val listOfImageViews = ArrayList<ImageView>()
+    private var tempLength = 0
+
+    override fun createViewModel(): BaseViewModel? {
+        cardPinViewModel = ViewModelProviders.of(this).get(CardPinViewModel::class.java)
+        return cardPinViewModel
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        binding = FragmentCardPinBinding.inflate(inflater, container, false)
+        activity!!.window.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_NOTHING)
+
+        binding.apply {
+            viewModel = cardPinViewModel
+
+            pinInputField.addTextChangeListener(object: MaskedTextChangedListener.ValueListener {
+                override fun onTextChanged(maskFilled: Boolean, extractedValue: String) {
+                    updateView(extractedValue)
+
+                    if (extractedValue.length == EngageAppConfig.cardPinLength) {
+                        // must call this after updateView() is done. This allows drawable.background to complete before
+                        // running this code so that all drawables are drawn
+                        Handler().post {
+                            when (cardPinViewModel.cardPinStateObservable.value) {
+                                CardPinViewModel.CardPinState.ENTER_PIN -> {
+                                    cardPinViewModel.validatePin(extractedValue)
+                                }
+                                CardPinViewModel.CardPinState.CONFIRM_PIN -> {
+                                    cardPinViewModel.submit(pinInputField.inputText.toString())
+                                }
+                                else -> {
+                                    cardPinViewModel.validatePin(extractedValue)
+                                }
+                            }
+                        }
+                    }
+                }
+            })
+
+            listOfImageViews.add(iconImageView1)
+            listOfImageViews.add(iconImageView2)
+            listOfImageViews.add(iconImageView3)
+            listOfImageViews.add(iconImageView4)
+
+            pinLayout.setOnClickListener {
+                // requestFocus to show keyboard in case keyboard was dismissed
+                pinInputField.clearFocus()
+                pinInputField.requestFocusOnEditInput()
+            }
+        }
+
+        cardPinViewModel.apply {
+            cardPinStateObservable.observe(this@CardPinFragment, Observer {
+                when (it) {
+                    CardPinViewModel.CardPinState.MISMATCH_PIN -> {
+                        val drawable = ContextCompat.getDrawable(context!!, R.drawable.card_pin_error_dot_shape)!!
+                        drawable.setTint(Palette.errorColor)
+                        updateView(getString(R.string.card_pin_mismatch_error_message),
+                                Palette.errorColor,
+                                drawable)
+                    }
+                    CardPinViewModel.CardPinState.CONFIRM_PIN -> {
+                        updateView(getString(R.string.card_pin_choose_confirmation),
+                                ContextCompat.getColor(context!!, R.color.textPrimary),
+                                ContextCompat.getDrawable(context!!, R.drawable.card_pin_unselected_dot_shape)!!)
+                    }
+                    CardPinViewModel.CardPinState.INVALID_PIN -> {
+                        val drawable = ContextCompat.getDrawable(context!!, R.drawable.card_pin_error_dot_shape)!!
+                        drawable.setTint(Palette.errorColor)
+                        updateView(getString(R.string.card_pin_choose_description),
+                                Palette.errorColor,
+                                drawable)
+                    }
+                    else -> {}
+                }
+            })
+
+            dialogInfoObservable.observe(this@CardPinFragment, Observer {
+                when (it.dialogType) {
+                    DialogInfo.DialogType.GENERIC_SUCCESS -> {
+                        //TODO(aHashimi): add show generic success before poping fragment off, currently code in PR
+                        binding.root.findNavController().popBackStack()
+                    }
+                    else -> {}
+                }
+            })
+        }
+
+        return binding.root
+    }
+
+    override fun onResume() {
+        super.onResume()
+        binding.pinInputField.requestFocusOnEditInput()
+    }
+
+    private fun updateView(text: String, @ColorInt textColor: Int, drawable: Drawable) {
+        binding.apply {
+            chooseDescriptionTextView.text = text
+            chooseDescriptionTextView.setTextColor(textColor)
+            // reset field
+            pinInputField.inputText = ""
+        }
+
+        for (listOfView in listOfImageViews) {
+            listOfView.background = drawable
+        }
+    }
+
+    private fun updateView(extractedValue: String) {
+        if (extractedValue.length in 0..EngageAppConfig.cardPinLength) {
+
+            if (extractedValue.length > tempLength) {
+                pinDigitAdded(extractedValue.length - 1)
+            } else if (extractedValue.length < tempLength) {
+                pinDigitDeleted(tempLength - 1)
+            }
+            tempLength = extractedValue.length
+        }
+    }
+
+    private fun pinDigitAdded(index: Int) {
+        val drawable = ContextCompat.getDrawable(context!!, R.drawable.card_pin_selected_dot_shape)!!
+        DrawableCompat.setTint(drawable, Palette.primaryColor)
+        listOfImageViews[index].background = drawable
+    }
+
+    private fun pinDigitDeleted(index: Int) {
+        listOfImageViews[index].background = ContextCompat.getDrawable(context!!, R.drawable.card_pin_unselected_dot_shape)
+    }
+}
--- OneToMany/app/src/onboarding/java/com/engageft/fis/pscu/feature/CardPinViewModel.kt
+++ OneToMany/app/src/onboarding/java/com/engageft/fis/pscu/feature/CardPinViewModel.kt
@@ -1,37 +1,35 @@
 package com.engageft.fis.pscu.feature
 
-import android.os.Handler
-import android.util.Log
-import androidx.databinding.Observable
-import androidx.databinding.ObservableField
 import androidx.lifecycle.MutableLiveData
 import com.engageft.apptoolbox.util.isDigitsOnly
-import com.engageft.fis.pscu.feature.BaseEngageViewModel
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
 import com.engageft.engagekit.EngageService
 import com.engageft.fis.pscu.config.EngageAppConfig
-import io.reactivex.disposables.CompositeDisposable
 
 
 class CardPinViewModel: BaseEngageViewModel() {
 
-    enum class CardPinFlow {
-        INVALID_PIN,
-        CONFIRM_PIN,
+    enum class CardPinState {
         MISMATCH_PIN,
-        SUBMIT_PIN
+        CONFIRM_PIN,
+        ENTER_PIN,
+        INVALID_PIN
+    }
+    private var pinEntered = ""
+    val cardPinStateObservable = MutableLiveData<CardPinState>()
+
+    init {
+        cardPinStateObservable.value = CardPinState.ENTER_PIN
     }
 
-    val flowObservable = MutableLiveData<CardPinFlow>()
-
-    fun submit(pin: Int, confirmPin: Int) {
-        if (pin.toString().length == EngageAppConfig.cardPinLength && pin.toString().isDigitsOnly() && pin == confirmPin) {
+    fun submit(pin: String) {
+        if (pinEntered.length == EngageAppConfig.cardPinLength && pin.isDigitsOnly() && pin == pinEntered) {
             val currentCard = EngageService.getInstance().storageManager.currentCard
 
             progressOverlayShownObservable.value = true
             compositeDisposable.add(
-                    EngageService.getInstance().setCardPinObservable(currentCard, pin)
+                    EngageService.getInstance().setCardPinObservable(currentCard, pin.toInt())
                             .subscribeOn(Schedulers.io())
                             .observeOn(AndroidSchedulers.mainThread())
                             .subscribe({ response ->
@@ -47,18 +45,17 @@ class CardPinViewModel: BaseEngageViewModel() {
                             })
             )
         } else {
-            Handler().post {
-                flowObservable.value = CardPinFlow.MISMATCH_PIN
-            }
+            cardPinStateObservable.value = CardPinState.MISMATCH_PIN
         }
     }
-}
-class CardPinDialogInfo(title: String? = null,
-                      message: String? = null,
-                      tag: String? = null,
-                      dialogType: DialogType = DialogType.GENERIC_ERROR,
-                      var cardPinDialogType: CardPinDialogInfo.CardPinDialogType = CardPinDialogType.PIN_VALIDATION_ERROR) : DialogInfo(title, message, tag, dialogType) {
-    enum class CardPinDialogType {
-        PIN_VALIDATION_ERROR
+
+    fun validatePin(pin: String) {
+        pinEntered = pin
+        if (pin.isDigitsOnly() && pin.length == EngageAppConfig.cardPinLength) {
+            cardPinStateObservable.value = CardPinState.CONFIRM_PIN
+        } else {
+            // this is redundant for CardPinFragment, technically this should not happen
+            cardPinStateObservable.value = CardPinState.INVALID_PIN
+        }
     }
 }
