791322b1ebcf4c12a08e5ba8980678c034be1621
Atia <atia.hashimi@engageft.com>
2018-12-03 17:09:06 -0800

    SHOW-362, refactored code so that if the PINs are a mismatch, the view goes back to its original state.

--- OneToMany/app/src/cardmanagement/java/com/engageft/fis/pscu/feature/CardPinViewModel.kt
+++ OneToMany/app/src/cardmanagement/java/com/engageft/fis/pscu/feature/CardPinViewModel.kt
@@ -2,33 +2,91 @@ package com.engageft.fis.pscu.feature
 
 import androidx.lifecycle.MutableLiveData
 import com.engageft.apptoolbox.util.isDigitsOnly
+import com.engageft.engagekit.EngageService
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
-import com.engageft.engagekit.EngageService
-import com.engageft.fis.pscu.config.EngageAppConfig
-
+/**
+ * CardPinViewModel
+ * <p>
+ * ViewModel for handling changing card PIN number
+ * </p>
+ * Created by Atia Hashimi on 12/3/18.
+ * Copyright (c) 2018 Engage FT. All rights reserved.
+ */
 class CardPinViewModel: BaseEngageViewModel() {
+
+    companion object {
+        private const val PIN_LENGTH = 4
+    }
 
     enum class CardPinState {
         MISMATCH_PIN,
         CONFIRM_PIN,
-        ENTER_PIN,
-        INVALID_PIN
+        INITIAL_ENTER_PIN,
+        INVALID_PIN,
     }
-    private var pinEntered = ""
+
+    enum class PinDigits {
+        DIGIT_ADDED,
+        DIGIT_DELETED
+    }
+
     val cardPinStateObservable = MutableLiveData<CardPinState>()
+    val cardPinDigitsState = MutableLiveData<Pair<PinDigits, Int>>()
+
+    var pinNumber: String = ""
+    set(value) {
+        val temp = field
+        field = value
+
+        if (field.length == 1 && cardPinStateObservable.value == CardPinState.MISMATCH_PIN) {
+            cardPinStateObservable.value = CardPinState.INITIAL_ENTER_PIN
+        }
+
+        if (field.length in 0..PIN_LENGTH) {
+            when {
+                field.length > temp.length -> cardPinDigitsState.value = Pair(PinDigits.DIGIT_ADDED, field.length - 1)
+                field.length < temp.length -> cardPinDigitsState.value = Pair(PinDigits.DIGIT_DELETED, temp.length - 1)
+            }
+
+            if (field.length == PIN_LENGTH) {
+                cardPinStateObservable.value = CardPinState.CONFIRM_PIN
+            }
+        }
+    }
+
+    var confirmPinNumber: String = ""
+    set(value) {
+        val temp = field
+        field = value
+
+        if (field.length in 0..PIN_LENGTH) {
+            when {
+                field.length > temp.length -> cardPinDigitsState.value = Pair(PinDigits.DIGIT_ADDED, field.length - 1)
+                field.length < temp.length -> cardPinDigitsState.value = Pair(PinDigits.DIGIT_DELETED, temp.length - 1)
+            }
+
+            if (field.length == PIN_LENGTH) {
+               if (field == pinNumber) {
+                   submit()
+               } else {
+                   cardPinStateObservable.value = CardPinState.MISMATCH_PIN
+               }
+            }
+        }
+    }
 
     init {
-        cardPinStateObservable.value = CardPinState.ENTER_PIN
+        cardPinStateObservable.value = CardPinState.INITIAL_ENTER_PIN
     }
 
-    fun submit(pin: String) {
-        if (pinEntered.length == EngageAppConfig.cardPinLength && pin.isDigitsOnly() && pin == pinEntered) {
+    private fun submit() {
+        if (isValidPin(pinNumber)) {
             val currentCard = EngageService.getInstance().storageManager.currentCard
 
             progressOverlayShownObservable.value = true
             compositeDisposable.add(
-                    EngageService.getInstance().setCardPinObservable(currentCard, pin.toInt())
+                    EngageService.getInstance().setCardPinObservable(currentCard, pinNumber.toInt())
                             .subscribeOn(Schedulers.io())
                             .observeOn(AndroidSchedulers.mainThread())
                             .subscribe({ response ->
@@ -44,17 +102,12 @@ class CardPinViewModel: BaseEngageViewModel() {
                             })
             )
         } else {
-            cardPinStateObservable.value = CardPinState.MISMATCH_PIN
-        }
-    }
-
-    fun validatePin(pin: String) {
-        pinEntered = pin
-        if (pin.isDigitsOnly() && pin.length == EngageAppConfig.cardPinLength) {
-            cardPinStateObservable.value = CardPinState.CONFIRM_PIN
-        } else {
             // this is redundant for CardPinFragment, technically this should not happen
             cardPinStateObservable.value = CardPinState.INVALID_PIN
         }
+    }
+
+    private fun isValidPin(pin: String): Boolean {
+        return pin.isDigitsOnly() && pin.length == PIN_LENGTH
     }
 }
