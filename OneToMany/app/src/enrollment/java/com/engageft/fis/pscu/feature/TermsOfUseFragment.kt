064e7933557f0f76741803fc3f34acf06d41d924
Atia <atia.hashimi@engageft.com>
2019-01-10 12:28:50 -0800

    FOTM-495, added Decline and Accept buttons & nav

--- OneToMany/app/src/enrollment/java/com/engageft/fis/pscu/feature/TermsOfUseFragment.kt
+++ OneToMany/app/src/enrollment/java/com/engageft/fis/pscu/feature/TermsOfUseFragment.kt
@@ -5,7 +5,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.lifecycle.ViewModelProviders
+import androidx.navigation.findNavController
 import com.engageft.apptoolbox.BaseViewModel
+import com.engageft.apptoolbox.view.InformationDialogFragment
+import com.engageft.fis.pscu.R
 import com.engageft.fis.pscu.databinding.FragmentTermsOfUseBinding
 import com.engageft.fis.pscu.feature.branding.Palette
 
@@ -26,8 +29,26 @@ class TermsOfUseFragment : BaseEngagePageFragment() {
     }
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         binding = FragmentTermsOfUseBinding.inflate(inflater, container, false)
-        binding.viewModel = enrollmentViewModel.termsOfUseDelegate
-        binding.palette = Palette
+        binding.apply {
+            viewModel = enrollmentViewModel.termsOfUseDelegate
+            palette = Palette
+
+            declineTermsButton.setOnClickListener {
+                fragmentDelegate.showDialog(infoDialogYesNoNewInstance(context!!,
+                        title = getString(R.string.ENROLLMENT_TERMS_OF_USE_ALERT_TITLE),
+                        message = getString(R.string.ENROLLMENT_TERMS_OF_USE_ALERT_MESSAGE),
+                        listener = object: InformationDialogFragment.InformationDialogFragmentListener {
+                            override fun onDialogFragmentNegativeButtonClicked() {}
+
+                            override fun onDialogFragmentPositiveButtonClicked() {
+                                binding.root.findNavController().popBackStack(R.id.getStartedFragment, false)
+                            }
+
+                            override fun onDialogCancelled() {}
+
+                        }))
+            }
+        }
         return binding.root
     }
 }
