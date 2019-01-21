ae320095e8917788a79e1483f43eeb5f978c8693
Joseph Hutchins <joseph.hutchins@engageft.com>
2019-01-21 13:01:33 -0800

    Linked/Active UI's.

--- OneToMany/app/src/enrollment/java/com/engageft/fis/pscu/feature/CardActiveFragment.kt
+++ OneToMany/app/src/enrollment/java/com/engageft/fis/pscu/feature/CardActiveFragment.kt
@@ -1,15 +1,20 @@
 package com.engageft.fis.pscu.feature
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import androidx.navigation.fragment.findNavController
 import com.engageft.apptoolbox.BaseViewModel
 import com.engageft.fis.pscu.R
 import com.engageft.fis.pscu.databinding.FragmentCardActivatedBinding
 import com.engageft.fis.pscu.feature.branding.Palette
+import com.engageft.fis.pscu.feature.palettebindings.applyBranding
+import com.ob.domain.lookup.branding.BrandingCard
 
 /**
  * CardActiveFragment
@@ -22,6 +27,9 @@ import com.engageft.fis.pscu.feature.branding.Palette
 class CardActiveFragment : BaseEngagePageFragment() {
     private lateinit var enrollmentViewModel: EnrollmentViewModel
     private lateinit var binding: FragmentCardActivatedBinding
+
+    private val brandingCardObserver = Observer<BrandingCard> { updateBrandingCard(it) }
+
     override fun createViewModel(): BaseViewModel? {
         enrollmentViewModel = ViewModelProviders.of(activity!!).get(EnrollmentViewModel::class.java)
         return enrollmentViewModel
@@ -31,9 +39,20 @@ class CardActiveFragment : BaseEngagePageFragment() {
         binding.viewModel = enrollmentViewModel
         binding.palette = Palette
 
-        binding.button1.setOnClickListener{
-            findNavController().navigate(R.id.action_cardActiveFragment_to_authenticatedActivity2)
-        }
+//        binding.button1.setOnClickListener{
+//            findNavController().navigate(R.id.action_cardActiveFragment_to_authenticatedActivity2)
+//        }
         return binding.root
+    }
+
+    private fun updateBrandingCard(brandingCard: BrandingCard?) {
+        brandingCard?.let {
+            binding.cardView.applyBranding(it, viewModel.compositeDisposable) { e ->
+                Toast.makeText(context, "Failed to retrieve card image", Toast.LENGTH_SHORT).show()
+                Log.e("BRANDING_INFO_FAIL", e.message)
+                //TODO(ttkachuk) right now it is not clear on how we should handle failure to retrieve the card image
+                //tracked in FOTM-497
+            }
+        }
     }
 }
