199503afb0572211ae9b4e7de0c08f35a1f7cef7
travis.tkachuk <travis.tkachuk@engageft.com>
2019-01-23 12:02:27 -0800

    FOTM-699 initial commit

---
+++ OneToMany/app/src/transactions/java/com/engageft/fis/pscu/feature/transactions/TransactionDetailsFragment.kt
@@ -0,0 +1,12 @@
+package com.engageft.fis.pscu.feature.transactions
+
+/**
+ * Transaction Details
+ * </p>
+ * TODO: Class Description
+ * </p>
+ * Created by Travis Tkachuk 1/23/19
+ * Copyright (c) 2019 Engage FT. All rights reserved.
+ */
+class TransactionDetailsFragment {
+}
