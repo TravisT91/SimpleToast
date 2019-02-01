b62fe70d3ed65a4b730ce763fd6de493b649ccca
Kurt Mueller <kurt.mueller@engageft.com>
2019-02-01 11:16:34 -0800

    FOTM-356 - updated to use databinding in recycler view for budget list items.

--- OneToMany/app/src/budgets/java/com/engageft/feature/budgets/recyclerview/BudgetItemViewHolder.kt
+++ OneToMany/app/src/budgets/java/com/engageft/feature/budgets/recyclerview/BudgetItemViewHolder.kt
@@ -1,8 +1,6 @@
 package com.engageft.feature.budgets.recyclerview
 
 import androidx.recyclerview.widget.RecyclerView
-import com.engageft.feature.budgets.BudgetConstants
-import com.engageft.fis.pscu.R
 import com.engageft.fis.pscu.databinding.RowBudgetTrackingPanelBinding
 import com.engageft.fis.pscu.feature.branding.Palette
 
@@ -28,23 +26,6 @@ class BudgetItemViewHolder(val binding: RowBudgetTrackingPanelBinding, listener:
 
     fun bind(budgetItem: BudgetItem) {
         binding.budgetItem = budgetItem
-        binding.rowBudgetTrackingPanel.apply {
-            setTitleText(budgetItem.title!!)
-            setRightSubTitle(budgetItem.spentString!!)
-            setRightSubtitleColor(budgetItem.spentStringColor)
-//            setProgress(budgetItem.progress)
-//            setProgressColor(budgetItem.progressColor)
-            setProgressBarHeight(budgetItem.progressBarHeight)
-            setIndicatorPosition(budgetItem.fractionTimePeriodPassed)
-
-            if (budgetItem.categoryName == BudgetConstants.CATEGORY_NAME_FE_TOTAL_SPENDING) {
-                showIndicatorText(true)
-                setIndicatorText(context.getString(R.string.budget_row_today))
-            } else {
-                showIndicatorText(false)
-            }
-        }
-
         binding.executePendingBindings()
     }
 }
