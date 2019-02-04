4268aaca592a1ad0132fac29335f44b9a483cb4b
Joseph Hutchins <joseph.hutchins@engageft.com>
2019-02-04 14:42:37 -0800

    Remove unused config items.

--- OneToMany/app/src/dashboard/java/com/engageft/fis/pscu/feature/DashboardConfig.kt
+++ OneToMany/app/src/dashboard/java/com/engageft/fis/pscu/feature/DashboardConfig.kt
@@ -9,9 +9,5 @@ package com.engageft.fis.pscu.feature
  * Copyright (c) 2018 Engage FT. All rights reserved.
  */
 object DashboardConfig {
-    const val CARD_MANAGEMENT_SHOW_CARD_DETAILS_ENABLED = true
-    const val CARD_MANAGEMENT_MOVE_MONEY_ENABLED = true
-    const val CARD_MANAGEMENT_CHANGE_PIN_ENABLED = true
 
-    const val FEATURE_ACH_OUTBOUND_TRANSFER_ENABLED = true
 }
